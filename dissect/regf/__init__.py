58b8f4d1d81f6fcd60d267cd17c7d0ff825c492e
pyrco <105293448+pyrco@users.noreply.github.com>
2023-02-07 09:40:35 +0100

    Add isort to flake8 linting (#11)
    (DIS-1789)

--- dissect/regf/__init__.py
+++ dissect/regf/__init__.py
@@ -1,6 +1,9 @@
-from dissect.regf.exceptions import Error, RegistryKeyNotFoundError, RegistryValueNotFoundError
+from dissect.regf.exceptions import (
+    Error,
+    RegistryKeyNotFoundError,
+    RegistryValueNotFoundError,
+)
 from dissect.regf.regf import RegistryHive
-
 
 __all__ = [
     "RegistryHive",
